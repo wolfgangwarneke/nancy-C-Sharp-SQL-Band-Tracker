2444c581647d7ec7a42bc48b59c5457dbf6bc803
Wolfgang Warneke <wolfgang.warneke@gmail.com>
2016-07-27 00:11:55 -0700

    add constructors, setters, and getters for band and venue classes

--- Objects/Band.cs
+++ Objects/Band.cs
@@ -34,179 +34,179 @@ namespace BandTracker
     {
       return _name;
     }
-
-    public static List<Band> GetAll()
-    {
-      List<Band> everyBand = new List<Band>{};
-      SqlConnection conn = DB.Connection();
-      SqlDataReader rdr = null;
-      conn.Open();
-      SqlCommand cmd = new SqlCommand("SELECT * FROM bands;", conn);
-      rdr = cmd.ExecuteReader();
-      while(rdr.Read())
-      {
-        int bandId = rdr.GetInt32(0);
-        string bandName = rdr.GetString(1);
-        Band readBand = new Band(bandName, bandId);
-        everyBand.Add(readBand);
-      }
-      if (rdr != null)
-      {
-        rdr.Close();
-      }
-      if (conn != null)
-      {
-        conn.Close();
-      }
-      return everyBand;
-    }
-    public void Save()
-    {
-      SqlConnection conn = DB.Connection();
-      conn.Open();
-      SqlDataReader rdr = null;
-      SqlCommand cmd = new SqlCommand ("INSERT INTO bands (name) OUTPUT INSERTED.id VALUES (@BandName);", conn);
-      SqlParameter nameParameter = new SqlParameter();
-      nameParameter.ParameterName = "@BandName";
-      nameParameter.Value = this.GetName();
-      cmd.Parameters.Add(nameParameter);
-      rdr = cmd.ExecuteReader();
-      while (rdr.Read())
-      {
-       this._id = rdr.GetInt32(0);
-      }
-      if (rdr != null)
-      {
-       rdr.Close();
-      }
-      if (conn != null)
-      {
-       conn.Close();
-      }
-    }
-    public static Band Find (int bandId)
-    {
-      List<Band> matchingBands = new List<Band> {};
-      SqlConnection conn = DB.Connection();
-      conn.Open();
-      SqlDataReader rdr = null;
-      SqlCommand cmd = new SqlCommand ("SELECT * FROM bands WHERE id = @BandId;", conn);
-      SqlParameter nameParameter = new SqlParameter ();
-      nameParameter.ParameterName = "@BandId";
-      nameParameter.Value = bandId;
-      cmd.Parameters.Add(nameParameter);
-      rdr = cmd.ExecuteReader();
-      while (rdr.Read())
-      {
-        int id = rdr.GetInt32(0);
-        string name = rdr.GetString(1);
-        Band newBand = new Band (name, id);
-        matchingBands.Add(newBand);
-      }
-      if (rdr != null)
-      {
-        rdr.Close();
-      }
-      if (conn != null)
-      {
-        conn.Close();
-      }
-      return matchingBands[0];
-    }
-    public static Band FindByName (string bandName)
-    {
-      List<Band> matchingBands = new List<Band> {};
-      SqlConnection conn = DB.Connection();
-      conn.Open();
-      SqlDataReader rdr = null;
-      SqlCommand cmd = new SqlCommand ("SELECT * FROM bands WHERE name = @BandName;", conn);
-      SqlParameter nameParameter = new SqlParameter ();
-      nameParameter.ParameterName = "@BandName";
-      nameParameter.Value = bandName;
-      cmd.Parameters.Add(nameParameter);
-      rdr = cmd.ExecuteReader();
-      while (rdr.Read())
-      {
-        int id = rdr.GetInt32(0);
-        string name = rdr.GetString(1);
-        Band newBand = new Band (name, id);
-        matchingBands.Add(newBand);
-      }
-      if (rdr != null)
-      {
-        rdr.Close();
-      }
-      if (conn != null)
-      {
-        conn.Close();
-      }
-      return matchingBands[0];
-    }
-    public static void DeleteAll()
-    {
-      SqlConnection conn = DB.Connection();
-      conn.Open();
-      SqlCommand cmd = new SqlCommand ("DELETE FROM bands;", conn);
-      cmd.ExecuteNonQuery();
-    }
-    public void DeleteThis()
-    {
-      SqlConnection conn = DB.Connection();
-      conn.Open();
-      SqlCommand cmd = new SqlCommand ("DELETE FROM bands WHERE id = @BandId;", conn);
-      SqlParameter idParameter = new SqlParameter ();
-      idParameter.ParameterName = "@BandId";
-      idParameter.Value = this.GetId();
-      cmd.Parameters.Add(idParameter);
-      cmd.ExecuteNonQuery();
-    }
-
-    public void UpdateBandName(string newName)
-    {
-      SqlConnection conn = DB.Connection();
-      SqlDataReader rdr;
-      conn.Open();
-
-      SqlCommand cmd = new SqlCommand("UPDATE bands SET name = @NewBandName OUTPUT INSERTED.name WHERE id = @BandId;", conn);
-
-      SqlParameter newNameParameter = new SqlParameter();
-      newNameParameter.ParameterName = "@NewBandName";
-      newNameParameter.Value = newName;
-      cmd.Parameters.Add(newNameParameter);
-
-      SqlParameter bandIdParameter = new SqlParameter();
-      bandIdParameter.ParameterName = "@BandId";
-      bandIdParameter.Value = this.GetId();
-      cmd.Parameters.Add(bandIdParameter);
-      rdr = cmd.ExecuteReader();
-
-      while(rdr.Read())
-      {
-        this._name = rdr.GetString(0);
-      }
-      if (conn != null)
-      {
-        conn.Close();
-      }
-      if (rdr != null)
-      {
-        rdr.Close();
-      }
-    }
-
-    public override bool Equals(System.Object otherBand)
-    {
-      if (otherBand is Band)
-      {
-       Band newBand = (Band) otherBand;
-       bool idEquality = (this.GetId() == newBand.GetId());
-       bool nameEquality = (this.GetName() == newBand.GetName());
-       return (idEquality && nameEquality);
-      }
-      else
-      {
-       return false;
-      }
-    }
+    //
+    // public static List<Band> GetAll()
+    // {
+    //   List<Band> everyBand = new List<Band>{};
+    //   SqlConnection conn = DB.Connection();
+    //   SqlDataReader rdr = null;
+    //   conn.Open();
+    //   SqlCommand cmd = new SqlCommand("SELECT * FROM bands;", conn);
+    //   rdr = cmd.ExecuteReader();
+    //   while(rdr.Read())
+    //   {
+    //     int bandId = rdr.GetInt32(0);
+    //     string bandName = rdr.GetString(1);
+    //     Band readBand = new Band(bandName, bandId);
+    //     everyBand.Add(readBand);
+    //   }
+    //   if (rdr != null)
+    //   {
+    //     rdr.Close();
+    //   }
+    //   if (conn != null)
+    //   {
+    //     conn.Close();
+    //   }
+    //   return everyBand;
+    // }
+    // public void Save()
+    // {
+    //   SqlConnection conn = DB.Connection();
+    //   conn.Open();
+    //   SqlDataReader rdr = null;
+    //   SqlCommand cmd = new SqlCommand ("INSERT INTO bands (name) OUTPUT INSERTED.id VALUES (@BandName);", conn);
+    //   SqlParameter nameParameter = new SqlParameter();
+    //   nameParameter.ParameterName = "@BandName";
+    //   nameParameter.Value = this.GetName();
+    //   cmd.Parameters.Add(nameParameter);
+    //   rdr = cmd.ExecuteReader();
+    //   while (rdr.Read())
+    //   {
+    //    this._id = rdr.GetInt32(0);
+    //   }
+    //   if (rdr != null)
+    //   {
+    //    rdr.Close();
+    //   }
+    //   if (conn != null)
+    //   {
+    //    conn.Close();
+    //   }
+    // }
+    // public static Band Find (int bandId)
+    // {
+    //   List<Band> matchingBands = new List<Band> {};
+    //   SqlConnection conn = DB.Connection();
+    //   conn.Open();
+    //   SqlDataReader rdr = null;
+    //   SqlCommand cmd = new SqlCommand ("SELECT * FROM bands WHERE id = @BandId;", conn);
+    //   SqlParameter nameParameter = new SqlParameter ();
+    //   nameParameter.ParameterName = "@BandId";
+    //   nameParameter.Value = bandId;
+    //   cmd.Parameters.Add(nameParameter);
+    //   rdr = cmd.ExecuteReader();
+    //   while (rdr.Read())
+    //   {
+    //     int id = rdr.GetInt32(0);
+    //     string name = rdr.GetString(1);
+    //     Band newBand = new Band (name, id);
+    //     matchingBands.Add(newBand);
+    //   }
+    //   if (rdr != null)
+    //   {
+    //     rdr.Close();
+    //   }
+    //   if (conn != null)
+    //   {
+    //     conn.Close();
+    //   }
+    //   return matchingBands[0];
+    // }
+    // public static Band FindByName (string bandName)
+    // {
+    //   List<Band> matchingBands = new List<Band> {};
+    //   SqlConnection conn = DB.Connection();
+    //   conn.Open();
+    //   SqlDataReader rdr = null;
+    //   SqlCommand cmd = new SqlCommand ("SELECT * FROM bands WHERE name = @BandName;", conn);
+    //   SqlParameter nameParameter = new SqlParameter ();
+    //   nameParameter.ParameterName = "@BandName";
+    //   nameParameter.Value = bandName;
+    //   cmd.Parameters.Add(nameParameter);
+    //   rdr = cmd.ExecuteReader();
+    //   while (rdr.Read())
+    //   {
+    //     int id = rdr.GetInt32(0);
+    //     string name = rdr.GetString(1);
+    //     Band newBand = new Band (name, id);
+    //     matchingBands.Add(newBand);
+    //   }
+    //   if (rdr != null)
+    //   {
+    //     rdr.Close();
+    //   }
+    //   if (conn != null)
+    //   {
+    //     conn.Close();
+    //   }
+    //   return matchingBands[0];
+    // }
+    // public static void DeleteAll()
+    // {
+    //   SqlConnection conn = DB.Connection();
+    //   conn.Open();
+    //   SqlCommand cmd = new SqlCommand ("DELETE FROM bands;", conn);
+    //   cmd.ExecuteNonQuery();
+    // }
+    // public void DeleteThis()
+    // {
+    //   SqlConnection conn = DB.Connection();
+    //   conn.Open();
+    //   SqlCommand cmd = new SqlCommand ("DELETE FROM bands WHERE id = @BandId;", conn);
+    //   SqlParameter idParameter = new SqlParameter ();
+    //   idParameter.ParameterName = "@BandId";
+    //   idParameter.Value = this.GetId();
+    //   cmd.Parameters.Add(idParameter);
+    //   cmd.ExecuteNonQuery();
+    // }
+    //
+    // public void UpdateBandName(string newName)
+    // {
+    //   SqlConnection conn = DB.Connection();
+    //   SqlDataReader rdr;
+    //   conn.Open();
+    //
+    //   SqlCommand cmd = new SqlCommand("UPDATE bands SET name = @NewBandName OUTPUT INSERTED.name WHERE id = @BandId;", conn);
+    //
+    //   SqlParameter newNameParameter = new SqlParameter();
+    //   newNameParameter.ParameterName = "@NewBandName";
+    //   newNameParameter.Value = newName;
+    //   cmd.Parameters.Add(newNameParameter);
+    //
+    //   SqlParameter bandIdParameter = new SqlParameter();
+    //   bandIdParameter.ParameterName = "@BandId";
+    //   bandIdParameter.Value = this.GetId();
+    //   cmd.Parameters.Add(bandIdParameter);
+    //   rdr = cmd.ExecuteReader();
+    //
+    //   while(rdr.Read())
+    //   {
+    //     this._name = rdr.GetString(0);
+    //   }
+    //   if (conn != null)
+    //   {
+    //     conn.Close();
+    //   }
+    //   if (rdr != null)
+    //   {
+    //     rdr.Close();
+    //   }
+    // }
+    //
+    // public override bool Equals(System.Object otherBand)
+    // {
+    //   if (otherBand is Band)
+    //   {
+    //    Band newBand = (Band) otherBand;
+    //    bool idEquality = (this.GetId() == newBand.GetId());
+    //    bool nameEquality = (this.GetName() == newBand.GetName());
+    //    return (idEquality && nameEquality);
+    //   }
+    //   else
+    //   {
+    //    return false;
+    //   }
+    // }
   }
 }
